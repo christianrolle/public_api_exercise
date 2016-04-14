0ec8011e97f1042d981b985808c1ddc961a2cdf4
Christian Rolle <rolle.christian@gmail.com>
2016-04-14 08:38:18 +0200

    Adds validation for presence of organization's public name.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ActiveRecord::Base
 
-  validates :name, presence: true
+  validates :name, :public_name, presence: true
 
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Organization, type: :model do
   describe "validations" do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:public_name) }
   end
 end
