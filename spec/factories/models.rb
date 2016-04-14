8c91114f2610a2e028c452c7e92a56ff34958a41
Christian Rolle <rolle.christian@gmail.com>
2016-04-14 11:35:51 +0200

    Fixed model factory model_slug attribute due to the required examples.

--- spec/factories/models.rb
+++ spec/factories/models.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     #association :organization, factory: :organization, strategy: :build
     association :organization, strategy: :build
     name "serie_1"
-    model_slug "serie-1"
+    model_slug "serie_1"
   end
 end
