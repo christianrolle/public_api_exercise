110097ea9209f2f7ea160f7aebde4e4606584e8c
Christian Rolle <rolle.christian@gmail.com>
2016-04-15 21:50:10 +0200

    Validates presence of the model types's model.

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -2,7 +2,7 @@ class ModelType < ActiveRecord::Base
 
   belongs_to :model
   
-  validates :name, :model_type_slug, presence: true
+  validates :name, :model_type_slug, :model, presence: true
   validates :base_price, presence: true, 
     numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
