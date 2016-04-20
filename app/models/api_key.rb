6f6f8869a6b30f6d9561a32c2465070002f0a3f6
Christian Rolle <rolle.christian@gmail.com>
2016-04-20 13:54:00 +0200

    API key model setting a uniq token before its creation. It is generated randomly until it is unique and then assigned and saved.

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -6,7 +6,7 @@ class ApiKey < ActiveRecord::Base
 
   def set_uniq_access_token
     begin
-      self.access_token = SecureRandom.hex
+      self.access_token ||= SecureRandom.hex
     end while self.class.exists?(access_token: access_token)
   end
 
