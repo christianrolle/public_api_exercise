3bfab2ddadb918acb0f0a12e9737d3da3b584413
Christian Rolle <rolle.christian@gmail.com>
2016-04-21 12:16:27 +0200

    Expects restricting API access due to the sent authentication token.

--- spec/controllers/models_controller_spec.rb
+++ spec/controllers/models_controller_spec.rb
@@ -9,4 +9,7 @@ RSpec.describe ModelsController, type: :controller do
     it { should route(:get, "/models/serie_3/model_types")
             .to(action: :index, model_slug: "serie_3") }
   end
+
+  it { should use_before_action(:restrict_public_api_access) }
+
 end
