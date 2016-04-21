dfdf209933c6120bca4a973f3e64497dc556bacb
Christian Rolle <rolle.christian@gmail.com>
2016-04-21 15:16:09 +0200

    Adds trait to model factory having a list of model types.

--- spec/factories/models.rb
+++ spec/factories/models.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     association :organization, strategy: :build
     name "serie_1"
     model_slug "serie_1"
+
+    trait :with_model_types do
+      after(:build) do |model|
+        model.model_types = build_list(:model_type, 1)
+      end
+    end
   end
 end
