95b028168e213a969a078a44a62c91abae3c40c9
Christian Rolle <rolle.christian@gmail.com>
2016-04-22 22:21:35 +0200

    Forwards total_price calculation to dedicated pricing policy object depending on the organizations pricing policy.

--- app/models/concerns/priced_model_type.rb
+++ app/models/concerns/priced_model_type.rb
@@ -6,8 +6,19 @@ class PricedModelType
     @model_type = model_type
   end
 
+  def total_price
+    return if model_type.base_price.nil?
+    pricing_policy_type.total_price model_type.base_price
+  end
+
   private
 
   attr_reader :model_type
+
+  delegate :model, to: :model_type
+
+  def pricing_policy_type
+    PricingPolicy.for model.pricing_policy
+  end
 
 end
--- spec/models/concerns/priced_model_type_spec.rb
+++ spec/models/concerns/priced_model_type_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe PricedModelType do
   describe "delegations" do
     it { is_expected.to delegate_method(:name).to(:model_type) }
   end
+
+  describe "#total_price" do
+    let!(:pricing_policy) { PricingPolicy::Fixed.new }
+    before {
+      allow(model_type.model).to receive(:pricing_policy).and_return('Fixed')
+    }
+
+    it "should return a price with fixed margin" do
+      total_price = model_type.base_price + 10
+      allow(pricing_policy).to receive(:total_price).and_return(total_price)
+      expect(PricingPolicy).to receive(:for).and_return(pricing_policy)
+
+      expect(subject.total_price).to eq(total_price)
+    end
+
+    context "when model_type has no defined base_price" do
+      it "should return nil" do
+        model_type.base_price = nil
+        expect(subject.total_price).to be_nil
+      end
+    end
+  end
 end
