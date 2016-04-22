93c7e21fb9d331a7faf5e5f0afdb07802afab2a1
Christian Rolle <rolle.christian@gmail.com>
2016-04-22 16:10:19 +0200

    Refactors loading HTML document without mocking.

--- spec/models/concerns/pricing_policy/web_margin.rb
+++ spec/models/concerns/pricing_policy/web_margin.rb
@@ -1,16 +1,11 @@
 require 'rails_helper'
 
-module Test
-  HTML = File.open(Rails.root.join "spec/support/documents/test.html")
-end
-
 RSpec.describe PricingPolicy::WebMargin do
 
-  subject { PricingPolicy::WebMargin.new('uri') }
+  let!(:uri) { Rails.root.join "spec/support/documents/test.html" }
+  subject { PricingPolicy::WebMargin.new(uri) }
 
   describe "#count" do
-    before { allow(subject).to receive(:document).and_return(Test::HTML) }
-
     it "should return count the occurences of 'status' in the HTML" do
       expect(subject.count 'status').to eq(9)
     end
