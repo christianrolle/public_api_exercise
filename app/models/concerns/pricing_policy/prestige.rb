9bd9b16fbf51eacd107270b8af27fbf2f808ce8e
Christian Rolle <rolle.christian@gmail.com>
2016-04-22 17:04:06 +0200

    Expects #total_price to return the base_price (so no margin), if the request to the external service failed for some reason.

--- app/models/concerns/pricing_policy/prestige.rb
+++ app/models/concerns/pricing_policy/prestige.rb
@@ -6,11 +6,15 @@ module PricingPolicy
     RSS_URI = 'http://www.yourlocalguardian.co.uk/sport/rugby/rss/'
 
     def margin base_price
+      document = request_document
+      return 0 if document.nil?
       document.entries.select(&:published).count
     end
 
-    def document
+    def request_document
       Feedjira::Feed.fetch_and_parse(RSS_URI)
+      rescue StandardError
     end
+
   end
 end
--- spec/models/concerns/pricing_policy/prestige_spec.rb
+++ spec/models/concerns/pricing_policy/prestige_spec.rb
@@ -6,16 +6,23 @@ module Test
 end
 
 RSpec.describe PricingPolicy::Prestige do
-
+  
   subject { PricingPolicy::Prestige.new }
 
   describe "#total_price" do
     let!(:base_price) { 1000 }
-    before { allow(subject).to receive(:document).and_return(Test::RSS) }
     
     it "should return the base_price with a prestige margin" do
+      allow(subject).to receive(:request_document).and_return(Test::RSS)
       total_price = base_price + 50
       expect(subject.total_price base_price).to eq(total_price)
+    end
+
+    context "when external service is unavailable" do
+      it "should return zero" do
+        stub_const("PricingPolicy::Prestige::RSS_URI", '')
+        expect(subject.total_price base_price).to eq(base_price)
+      end
     end
   end
 
