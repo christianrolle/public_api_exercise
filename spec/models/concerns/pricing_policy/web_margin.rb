75a77b5adfba2dfacf15dc492d896163723822c1
Christian Rolle <rolle.christian@gmail.com>
2016-04-22 15:43:11 +0200

    Refactors loading the HTML test document. By moving it into a constant out of the test suite, it is loaded only once.

--- spec/models/concerns/pricing_policy/web_margin.rb
+++ spec/models/concerns/pricing_policy/web_margin.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
+module Test
+  HTML = File.open(Rails.root.join "spec/support/documents/test.html")
+end
+
 RSpec.describe PricingPolicy::WebMargin do
 
   subject { PricingPolicy::WebMargin.new('uri') }
 
   describe "#count" do
-    let(:html) { File.open(Rails.root.join "spec/support/documents/test.html") }
-    before { allow(subject).to receive(:document).and_return(html) }
+    before { allow(subject).to receive(:document).and_return(Test::HTML) }
 
     it "should return count the occurences of 'status' in the HTML" do
       expect(subject.count 'status').to eq(9)
